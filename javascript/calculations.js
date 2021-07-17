88eba8a548c4ca6718ff032c839ddb2a98d090ae
Nick Ramsay <NickRamsay1989@gmail.com>
2021-07-17 21:02:41 +1000

    Added tax rates

---
+++ javascript/calculations.js
@@ -0,0 +1,3 @@
+import taxRates from "./taxRules";
+
+window.onload(console.log(taxRates));
---
+++ javascript/taxRules.js
@@ -0,0 +1,39 @@
+export const taxRates = {
+    "resident-tax-brackets": [
+        {
+            "residency-status": "resident",
+            "min-income": 0,
+            "max-income": 18200,
+            "base-tax": 0,
+            "tax-rate": 0.0
+        },
+        {
+            "residency-status": "resident",
+            "min-income": 18201,
+            "max-income": 45000,
+            "base-tax": 0,
+            "tax-rate": 0.19
+        },
+        {
+            "residency-status": "resident",
+            "min-income": 45001,
+            "max-income": 120000,
+            "base-tax": 5092,
+            "tax-rate": 0.325
+        },
+        {
+            "residency-status": "resident",
+            "min-income": 120001,
+            "max-income": 180000,
+            "base-tax": 29467,
+            "tax-rate": 0.37
+        },
+        {
+            "residency-status": "resident",
+            "min-income": 180001,
+            "max-income": null,
+            "base-tax": 51667,
+            "tax-rate": 0.45
+        }
+    ]
+}
