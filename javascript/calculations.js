aa9d9f2faed4a804ed49b5c1dbf282095eed6716
Nick Ramsay <NickRamsay1989@gmail.com>
2021-07-22 20:43:00 +1000

    Tax calculator now fully functional

--- javascript/calculations.js
+++ javascript/calculations.js
@@ -1,3 +1,62 @@
+let taxBrackets = {
+    resident: {
+        2021: [
+            {
+                "min-income": 0,
+                "max-income": 18200,
+                "base-tax": 0,
+                "tax-rate": 0.0
+            },
+            {
+                "min-income": 18201,
+                "max-income": 45000,
+                "base-tax": 0,
+                "tax-rate": 0.19
+            },
+            {
+                "min-income": 45001,
+                "max-income": 120000,
+                "base-tax": 5092,
+                "tax-rate": 0.325
+            },
+            {
+                "min-income": 120001,
+                "max-income": 180000,
+                "base-tax": 29467,
+                "tax-rate": 0.37
+            },
+            {
+                "min-income": 180001,
+                "max-income": null,
+                "base-tax": 51667,
+                "tax-rate": 0.45
+            }
+        ]
+    },
+    nonresident: {
+        2021: [
+            {
+                "min-income": 0,
+                "max-income": 120000,
+                "base-tax": 0,
+                "tax-rate": 0.325
+            },
+            {
+                "min-income": 120001,
+                "max-income": 180000,
+                "base-tax": 39000,
+                "tax-rate": 0.37
+            },
+            {
+                "min-income": 180001,
+                "max-income": null,
+                "base-tax": 61200,
+                "tax-rate": 0.45
+            }
+        ]
+    }
+}
+
 let resident = document.getElementById("resident-button");
 let nonresident = document.getElementById("non-resident-button");
 let taxYear = document.getElementById("year-input");
@@ -5,23 +64,39 @@ let grossSalary = document.getElementById("gross-salary-input");
 
 const calculateTax = () => {
     if (grossSalary.value !== "") {
-        console.log(grossSalary.value);
-        console.log(resident.checked);
-        console.log(nonresident.checked);
-        console.log(taxYear.value);
+        
+        let applicableGrossSalary = grossSalary.value;
+        let applicableTaxYear = taxYear.value;
 
-        renderCalculations(10, 20, 30, 40, 53, 68, 79);
+        let applicableResidentStatus;
+        resident.checked === true ? applicableResidentStatus = "resident" : applicableResidentStatus = "nonresident";
+
+        let applicableTaxBracket = taxBrackets[applicableResidentStatus.toString()][applicableTaxYear.toString()].find(element => element["min-income"] <= applicableGrossSalary && (element["max-income"] >= applicableGrossSalary || element["max-income"] === null));
+
+        console.log(applicableTaxBracket);
+
+        let fixedTax = applicableTaxBracket["base-tax"];
+        let variableTax = (applicableGrossSalary - applicableTaxBracket["min-income"]) * applicableTaxBracket["tax-rate"];
+        let totalTax = fixedTax + variableTax;
+        let totalGrossSalary = applicableGrossSalary;
+        let totalNetSalary = totalGrossSalary - totalTax;
+        let totalNetMonthlySalary = totalNetSalary/12;
+        let totalNetWeeklySalary = totalNetSalary/52
+
+        console.log(totalGrossSalary);
+
+        renderCalculations(fixedTax, variableTax, totalTax, totalGrossSalary, totalNetSalary, totalNetMonthlySalary, totalNetWeeklySalary);
     };
 };
 
-const renderCalculations = (fixedTax, variableTax, totalTax, totalGrossSalary, totalNetSalary, totalMonthlySalary, totalWeeklySalary) => {
+const renderCalculations = (fixedTax, variableTax, totalTax, totalGrossSalary, totalNetSalary, totalNetMonthlySalary, totalNetWeeklySalary) => {
     document.getElementById("fixed-tax").innerHTML = fixedTax.toFixed(2);
     document.getElementById("variable-tax").innerHTML = variableTax.toFixed(2);
     document.getElementById("total-tax").innerHTML = totalTax.toFixed(2);
-    document.getElementById("total-gross-salary").innerHTML = totalGrossSalary.toFixed(2);
+    document.getElementById("total-gross-salary").innerHTML = Number(totalGrossSalary).toFixed(2);
     document.getElementById("total-net-salary").innerHTML = totalNetSalary.toFixed(2);
-    document.getElementById("total-monthly-salary").innerHTML = totalMonthlySalary.toFixed(2);
-    document.getElementById("total-weekly-salary").innerHTML = totalWeeklySalary.toFixed(2);
+    document.getElementById("total-net-monthly-salary").innerHTML = totalNetMonthlySalary.toFixed(2);
+    document.getElementById("total-net-weekly-salary").innerHTML = totalNetWeeklySalary.toFixed(2);
 };
 
 document.getElementById("calculate-tax-button").addEventListener("click", calculateTax);
